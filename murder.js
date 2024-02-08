e6951eaa3ef5e05fdf32a8650d6a502bd88afd67
AxisFray <113852846+AxisFray@users.noreply.github.com>
2024-02-08 19:47:40 +0100

    Add files via upload

--- murder.js
+++ murder.js
@@ -9,6 +9,12 @@
             var pie=80, ludz=0, narz=0, pos=0, zdr = 100;
             var cenaNarzedzi = 20, cenaLudzi = 250, cenaPos = 500,cenaZdr=100;
             var szankrad = 85, szanrab = 40, szanhack = 20;
+            var dokumenty1 = 0, ziola1 = 0, narzady1 = 0, ludzie1 = 0;
+
+            var hdokumenty = document.getElementById("dok1")
+            var hziola = document.getElementById("ziola1")
+            var hnarzady = document.getElementById("narz1")
+            var hludzie = document.getElementById("ludzie1")
 
             var szankrad1 = document.getElementById("kradnij")
             var szanrab1 = document.getElementById("rabuj")
@@ -27,7 +33,8 @@
             var ckdok = document.getElementById("cenadok")
             var cknarz = document.getElementById("cenanarzadow")
             var ckziola = document.getElementById("cenamar")
-
+            var ck = [ckdok, ckziola, cknarz, ckludzi]
+            var cs = [csdok,csziola,csnarz,csludzi]
             var csludzi = document.getElementById("cenasludzi")
             var csdok = document.getElementById("cenasdok")
             var csnarz = document.getElementById("cenasnarzadow")
@@ -57,7 +64,8 @@
                     Sync();
                 }
             }
-            function Sync() {
+function Sync() {
+    CCeny();
                 pieniadze.innerHTML = pie;
                 ludzie.innerHTML = ludz;
                 posiadlosci.innerHTML = pos;
@@ -67,6 +75,11 @@
                 szankrad1.innerHTML = szankrad;
                 szanrab1.innerHTML = szanrab;
                 szanhack1.innerHTML = szanhack;
+
+                hdokumenty.innerHTML = dokumenty1;
+                hziola.innerHTML = ziola1;
+                hnarzady.innerHTML = narzady1;
+                hludzie.innerHTML = ludzie1;
 
                 cenanarz.innerHTML = cenaNarzedzi;
                 cenacz.innerHTML = cenaLudzi;
@@ -98,6 +111,19 @@
             csludzi.innerHTML = cenysprzedazy[3]
 
 
+
+}
+
+
+            function CCeny() {
+                for (var i = 0; i < 4; i++) {
+                    los1 = Math.floor(Math.random() * (1 / 20) *cs[i])
+                    los2 = Math.floor(Math.random() * ( 1 / 20 ) * ck[i])
+                    if ((Math.floor(Math.random() * 2) + 1) == 1) { cs[i] -= los1 } else { cs[i] += los1 }
+                    if ((Math.floor(Math.random() * 2) + 1) == 1) { ck[i] -= los1 } else { ck[i] += los1 }
+
+
+                }
             }
             //SKLEP
             function Narzedzia() {
@@ -130,19 +156,16 @@
                 }
                 Sync();
             }
-            function Kup(arg){
-                switch(arg){
-                    case 
-                }
+            
 
 
 
 
-            }
+            
             function UlZdrowie(){
             if (pie < cenaZdr) { info.innerHTML = "Nie masz pieniedzy na ulepszenie zdrowia" }
             else{
-                money -= cenaZdr;
+                pie -= cenaZdr;
                 zdr += 20;
 
             }
@@ -152,7 +175,7 @@
                 let los1 = Math.floor(Math.random() * 100  ) + 1;
                 console.log(los1)
                 console.log(szankrad)
-                if (los1 >szankrad) { info.innerHTML = "Zosta�e� z�apany na kradzie�y !!!"; zdr -= 5; pie -= 25 }
+                if (los1 >szankrad) { info.innerHTML = "Zostales zlapany na kradziezy !!!"; zdr -= 5; pie -= 25 }
                 else {
                     pie += 20;
                 }
@@ -163,7 +186,7 @@
             function rabuj() {
                 let los1 = Math.floor(Math.random() * 100) + 1;
                 
-                if (los1 > szanrab) { info.innerHTML = "Zosta�e� z�apany na rabunku !!!"; zdr -= 10; pie -= 60 }
+                if (los1 > szanrab) { info.innerHTML = "Zostales zlapany na rabunku !!!"; zdr -= 10; pie -= 60 }
                 else { pie += 45; }
                 if (Math.random() * 30 == 1) { zdarzenie() }
                 Sync();
@@ -171,7 +194,7 @@
             function hackuj() {
                 let los1 = Math.floor(Math.random() * 100) + 1;
                 
-                if (los1 > szanhack) { info.innerHTML = "Zosta�e� z�apany na hackerstwie !!!"; zdr -= 15; pie -= 100 }
+                if (los1 > szanhack) { info.innerHTML = "Zostales zlapany na hackerstwie !!!"; zdr -= 15; pie -= 100 }
                 else {
                     pie += 80; 
                 }
@@ -186,36 +209,49 @@
             
             //CZARNY RYNEK
             function kup(aaa){
-                switch(a){
+                switch(aaa){
                     //KUP
                     case "kludz":
-                    if(pie>=cenykupna[3]){posLudzi+=1}
-                    else{info += "Nie masz pieniedzy na ludzi"}
+                        
+                        if (pie >= cenykupna[3]) { ludzie1 += 1; pie -= cenykupna[3] }
+                        else { info.innerHTML= "Nie masz pieniedzy na ludzi" }
+                        Sync();
                         break;
                     case "kziola":
-                        if(pie>=cenykupna[1]){posZiola+=1}
-                        else{info += "Nie masz pieniedzy na ziola"}
+                        if (pie >= cenykupna[1]) { ziola1 += 1;pie-=cenykupna[1] }
+                        else { info.innerHTML = "Nie masz pieniedzy na ziola" }
+                        Sync();
                         break;
                     case "kdok":
-                        if(pie>=cenykupna[0]){posDok+=1}
-                        else{info += "Nie masz pieniedzy na dokumenty"}
+                        if (pie >= cenykupna[0]) { dokumenty1 += 1;pie-=cenykupna[0] }
+                        else { info.innerHTML = "Nie masz pieniedzy na dokumenty" }
+                        Sync();
                         break;
                     case "knarz":
-                        if(pie>=cenykupna[2]){posNarz+=1}
-                        else{info += "Nie masz pieniedzy na narzady"}
+                        if (pie >= cenykupna[2]) { narzady1 += 1;pie-=cenykupna[2] }
+                        else { info.innerHTML = "Nie masz pieniedzy na narzady" }
+                        Sync();
                         break;
                         //SPRZEDAJ
                     case "sludz":
-                        if(posLudzi>=1){posLudzi-=1}
+                        if (ludzie1 >= 1) { ludzie1 -= 1; pie += cenysprzedazy[3] }
+                        else { info.innerHTML = "Nie masz ludzi" }
+                        Sync();
                         break;
                     case "sziola":
-
+                        if (ziola1 >= 1) { ziola1 -= 1; pie += cenysprzedazy[1] }
+                        else { info.innerHTML = "Nie masz ziola" }
+                        Sync();
                         break;
                     case "sdok":
-
+                        if (dokumenty1 >= 1) { dokumenty1 -= 1; pie += cenysprzedazy[0] }
+                        else { info.innerHTML = "Nie masz dokumentow" }
+                        Sync();
                         break;
                     case "snarz":
-
+                        if (narzady1 >= 1) { narzady1 -= 1; pie += cenysprzedazy[2] }
+                        else { info.innerHTML = "Nie masz narządów" }
+                        Sync();
                         break;
 
 
